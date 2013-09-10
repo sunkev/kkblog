4a98548eb889bce1ffbcbfe3efb4226a96948570
Kevin Sun <sunkev@brandeis.edu>
2013-09-10 17:11:33 -0400

    Deleted test folder, added author model.

--- db/migrate/20130910201639_create_blog_entries.rb
+++ db/migrate/20130910201639_create_blog_entries.rb
@@ -1,7 +1,10 @@
 class CreateBlogEntries < ActiveRecord::Migration
   def change
     create_table :blog_entries do |t|
-
+      t.string :title
+      t.text :body
+      t.integer :author_id
+      t.boolean :state :default false
       t.timestamps
     end
   end
